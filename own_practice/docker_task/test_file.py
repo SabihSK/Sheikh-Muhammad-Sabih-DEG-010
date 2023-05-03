36742157d733e90774f3ed3fd2fbfbb9cbb958a9
SheikhMuhammadSabih2303010KHIDEG <124855896+SheikhMuhammadSabih2303010KHIDEG@users.noreply.github.com>
2023-05-03 11:05:53 +0500

    practice

---
+++ own_practice/docker_task/test_file.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import seaborn
+import sklearn
+
+print("pandas version",pd.__version__)
+print("numpy version",np.__version__)
+print("seaborn version",seaborn.__version__)
+print("sklearn version",sklearn.__version__)
+
