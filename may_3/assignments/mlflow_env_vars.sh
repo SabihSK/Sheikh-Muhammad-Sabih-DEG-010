f426d79963e9d4a5b19c1f85c10c552a36c417c3
SheikhMuhammadSabih2303010KHIDEG <124855896+SheikhMuhammadSabih2303010KHIDEG@users.noreply.github.com>
2023-05-08 10:58:48 +0500

    assignment 3.4 has been added.

--- may_3/assignments/mlflow_env_vars.sh
+++ may_3/assignments/mlflow_env_vars.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 export MLFLOW_CONDA_HOME=/home/sabih/anaconda3/
-export MLFLOW_TRACKING_URI="http://0.0.0.0:5005"
+export MLFLOW_TRACKING_URI="http://0.0.0.0:5006"
 export MLFLOW_AR=./mlruns
