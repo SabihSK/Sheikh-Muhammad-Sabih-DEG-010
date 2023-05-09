94b1cf8ac0af589868c4f65d01e504640fa18f2b
SheikhMuhammadSabih2303010KHIDEG <124855896+SheikhMuhammadSabih2303010KHIDEG@users.noreply.github.com>
2023-05-09 11:04:50 +0500

    assignment 4.2 has been added.

--- may_8/assignments/create_kafka_topic.sh
+++ may_8/assignments/create_kafka_topic.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-docker compose exec broker kafka-topics --create --topic event1 --bootstrap-server broker:9092 --partitions 3
-
-docker compose exec broker kafka-topics --create --topic event2 --bootstrap-server broker:9092 --partitions 5
+docker compose exec broker kafka-topics --create --topic event1 --bootstrap-server broker:9092
 
 docker compose exec broker kafka-topics --list --bootstrap-server broker:9092
 
-docker compose exec broker kafka-topics --describe --bootstrap-server broker:9092
+docker compose exec broker kafka-topics --topic event1 --describe --bootstrap-server broker:9092
